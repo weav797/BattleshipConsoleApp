19abb32cb4ed746ca2b77febd5f4fbe88572a67c
Orion Weaver <awesomeboy7865@gmail.com>
2024-02-19 15:56:58 -0700

    Add project files.

---
+++ BattleshipLiteLibrary/Models/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattleshipLiteLibrary.Models
+{
+    public enum GridSpotStatus
+    {
+        Empty,
+        Ship,
+        Miss,
+        Hit,
+        Sunk
+    }
+}
